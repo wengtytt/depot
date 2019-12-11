9e9d36721784aed93dac2b296abbae79ddec0885
tianyu <tianyu@proximify.ca>
2019-12-10 20:49:59 -0500

    Updates

---
+++ precache-manifest.8ffc659621080f9bed1331527b808204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa737825b227ed4904bdcdf292cc794",
+    "url": "/depot/index.html"
+  },
+  {
+    "revision": "0034f72721f7c7a2ab34",
+    "url": "/depot/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "de9b63cae850d89e7c36",
+    "url": "/depot/static/js/2.bfc65748.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/depot/static/js/2.bfc65748.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0034f72721f7c7a2ab34",
+    "url": "/depot/static/js/main.7b2af41b.chunk.js"
+  },
+  {
+    "revision": "6b079b4db46e37dc31ab",
+    "url": "/depot/static/js/runtime-main.66e17b21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/depot/static/media/logo.5d5d9eef.svg"
+  }
+]);
